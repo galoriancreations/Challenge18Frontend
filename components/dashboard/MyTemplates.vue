cb5ee71789d99dd8e53a890880ad652a45c1529e
yinonhever <yinonehever@gmail.com>
2021-09-04 04:42:42 +0300

    button for creating new template

--- components/dashboard/MyTemplates.vue
+++ components/dashboard/MyTemplates.vue
@@ -4,6 +4,9 @@
       <p>Private templates you created will appear here.</p>
       <p>Click the button below to create a new template.</p>
     </div>
+    <template slot="button">
+      <ActionButton type="add" color="blue" @click="selectTemplate" />
+    </template>
   </DashboardSection>
 </template>
 
@@ -20,6 +23,21 @@ export default {
     },
     hasTemplates() {
       return this.user?.templates && this.templates.length > 0;
+    }
+  },
+  methods: {
+    selectTemplate(templateId) {
+      if (templateId) {
+        this.$cookies.set("templateId", templateId);
+      } else {
+        this.$cookies.remove("templateId");
+      }
+      this.$cookies.remove("challengeId");
+      this.$cookies.remove("draftId");
+      this.$router.push({
+        path: "/challenge-editor",
+        query: { templateOnly: true }
+      });
     }
   }
 };
