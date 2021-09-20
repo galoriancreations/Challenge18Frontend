64ed4f8d61614dc64fcc8ec60ae4ea4e197dcfc1
yinonhever <yinonehever@gmail.com>
2021-09-20 05:48:14 +0300

    _

--- components/home/TopChallenges.vue
+++ components/home/TopChallenges.vue
@@ -1,6 +1,6 @@
 <template>
   <WhiteSection class="top-challenges">
-    <SectionHeading small>Top 18 Challenges</SectionHeading>
+    <SectionHeading small>Top Challenges</SectionHeading>
     <div class="top-challenges__grid">
       <TopChallengesItem
         v-for="item in challenges"
@@ -39,90 +39,6 @@ export default {
           title: "Friendly AI",
           text:
             "Join us on an amazing process of 18 Days challenge of Friendly AI with each day focusing on a certain value of how to better live and shape our future."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoSDGs-01.png"),
-          title: "SDG's",
-          text:
-            "In 2015, world leaders agreed to 17 Global Goals (officially known as the Sustainable Development Goals or SDGs). It's now five years on, and we have more work than ever to do.\nThese goals have the power to create a better world by 2030, by ending poverty, fighting inequality and addressing the urgency of climate change. Guided by the goals, it is now up to all of us, governments, businesses, civil society and the general public to work together to build a better future for everyone.\nJoin us on an amazing process of 18 Days of Sustainability & Leadership Challenge inspired by the Global Goals."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoClimateAction-01.png"),
-          title: "Climate Action",
-          text:
-            "Take urgent action to combat climate change and its impacts. Join us on an on an amazing process of 18 Days of Climate Action Challenge inspired by Global Goals."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoSongsAndValues-01.png"),
-          title: "Songs & Values",
-          text:
-            "Join us on an amazing process of 18 Days challenge of Songs & Values with each day focusing on a certain value and ending with one big celebration and a playlist of songs we all love."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoFriendlyAI-01.png"),
-          title: "Friendly AI",
-          text:
-            "Join us on an amazing process of 18 Days challenge of Friendly AI with each day focusing on a certain value of how to better live and shape our future."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoSDGs-01.png"),
-          title: "SDG's",
-          text:
-            "In 2015, world leaders agreed to 17 Global Goals (officially known as the Sustainable Development Goals or SDGs). It's now five years on, and we have more work than ever to do.\nThese goals have the power to create a better world by 2030, by ending poverty, fighting inequality and addressing the urgency of climate change. Guided by the goals, it is now up to all of us, governments, businesses, civil society and the general public to work together to build a better future for everyone.\nJoin us on an amazing process of 18 Days of Sustainability & Leadership Challenge inspired by the Global Goals."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoClimateAction-01.png"),
-          title: "Climate Action",
-          text:
-            "Take urgent action to combat climate change and its impacts. Join us on an on an amazing process of 18 Days of Climate Action Challenge inspired by Global Goals."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoSongsAndValues-01.png"),
-          title: "Songs & Values",
-          text:
-            "Join us on an amazing process of 18 Days challenge of Songs & Values with each day focusing on a certain value and ending with one big celebration and a playlist of songs we all love."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoFriendlyAI-01.png"),
-          title: "Friendly AI",
-          text:
-            "Join us on an amazing process of 18 Days challenge of Friendly AI with each day focusing on a certain value of how to better live and shape our future."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoSDGs-01.png"),
-          title: "SDG's",
-          text:
-            "In 2015, world leaders agreed to 17 Global Goals (officially known as the Sustainable Development Goals or SDGs). It's now five years on, and we have more work than ever to do.\nThese goals have the power to create a better world by 2030, by ending poverty, fighting inequality and addressing the urgency of climate change. Guided by the goals, it is now up to all of us, governments, businesses, civil society and the general public to work together to build a better future for everyone.\nJoin us on an amazing process of 18 Days of Sustainability & Leadership Challenge inspired by the Global Goals."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoClimateAction-01.png"),
-          title: "Climate Action",
-          text:
-            "Take urgent action to combat climate change and its impacts. Join us on an on an amazing process of 18 Days of Climate Action Challenge inspired by Global Goals."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoSongsAndValues-01.png"),
-          title: "Songs & Values",
-          text:
-            "Join us on an amazing process of 18 Days challenge of Songs & Values with each day focusing on a certain value and ending with one big celebration and a playlist of songs we all love."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoFriendlyAI-01.png"),
-          title: "Friendly AI",
-          text:
-            "Join us on an amazing process of 18 Days challenge of Friendly AI with each day focusing on a certain value of how to better live and shape our future."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoSDGs-01.png"),
-          title: "SDG's",
-          text:
-            "In 2015, world leaders agreed to 17 Global Goals (officially known as the Sustainable Development Goals or SDGs). It's now five years on, and we have more work than ever to do.\nThese goals have the power to create a better world by 2030, by ending poverty, fighting inequality and addressing the urgency of climate change. Guided by the goals, it is now up to all of us, governments, businesses, civil society and the general public to work together to build a better future for everyone.\nJoin us on an amazing process of 18 Days of Sustainability & Leadership Challenge inspired by the Global Goals."
-        },
-        {
-          image: require("~/assets/images/logos/PNGChallenge18LogoClimateAction-01.png"),
-          title: "Climate Action",
-          text:
-            "Take urgent action to combat climate change and its impacts. Join us on an on an amazing process of 18 Days of Climate Action Challenge inspired by Global Goals."
         }
       ]
     };
@@ -148,9 +64,11 @@ export default {
 .top-challenges {
   &__grid {
     display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    gap: 3rem;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 4.5rem;
     align-items: center;
+    max-width: 90rem;
+    margin: auto;
 
     @include respond(mobile-land) {
       gap: 2.5rem;
