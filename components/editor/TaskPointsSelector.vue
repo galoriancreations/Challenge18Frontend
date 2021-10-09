ea85c81d7d89bdf6b490d69ad3c55f1b3aff52ea
yinonhever <yinonehever@gmail.com>
2021-10-09 07:00:25 +0300

    another attempt (targeting number input)

--- components/editor/TaskPointsSelector.vue
+++ components/editor/TaskPointsSelector.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="points-selector">
     <label class="task-form__selector-label">Awarded points</label>
-    <NumberInput
-      v-if="isTemplateEditable"
-      v-model="task.points"
-      :min="1"
-      :max="18"
-      :center="true"
-      size="large"
-      :disabled="!task.isBonus"
-      inline
-      controls
-    />
-    <span v-else>{{ task.points }}</span>
+    <client-only>
+      <NumberInput
+        v-if="isTemplateEditable"
+        v-model="task.points"
+        :min="1"
+        :max="18"
+        :center="true"
+        size="large"
+        :disabled="!task.isBonus"
+        inline
+        controls
+      />
+      <span v-else>{{ task.points }}</span>
+    </client-only>
   </div>
 </template>
 
 <script>
-import NumberInput from "@chenfengyuan/vue-number-input";
-
 export default {
-  components: { NumberInput },
+  components: {
+    NumberInput: () =>
+      process.client ? import("@chenfengyuan/vue-number-input") : null
+  },
   inject: ["task", "isTemplateEditable"]
 };
 </script>
