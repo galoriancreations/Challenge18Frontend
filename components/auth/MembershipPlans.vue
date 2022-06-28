c35072844d94344262d5ab76152f9cf7359bea2e
yinonhever <yinonehever@gmail.com>
2022-06-28 10:58:46 +0300

    more content updates

--- components/auth/MembershipPlans.vue
+++ components/auth/MembershipPlans.vue
@@ -19,24 +19,27 @@ export default {
 <style lang="scss">
 .plans-grid {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  // grid-template-columns: repeat(4, 1fr);
   gap: 2.5rem;
+  grid-template-columns: 1fr;
+  max-width: 40rem;
+  margin: auto;
 
   @include respond(tablet-land) {
-    grid-template-columns: 1fr 1fr;
-    gap: 5rem;
-    max-width: 75rem;
-    margin: auto;
+    // grid-template-columns: 1fr 1fr;
+    // gap: 5rem;
+    // max-width: 75rem;
+    // margin: auto;
   }
 
   @include respond(tablet-sm) {
-    gap: 2.5rem;
+    // gap: 2.5rem;
   }
 
   @include respond(mobile-land) {
-    grid-template-columns: 1fr;
-    gap: 4rem;
-    max-width: 40rem;
+    // grid-template-columns: 1fr;
+    // gap: 4rem;
+    // max-width: 40rem;
   }
 }
 </style>
