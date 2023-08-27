2a9481a41e263cf2f1c030449b9cd4eae4d08620
Elad Nulman <eladi483@gmail.com>
2023-08-27 19:39:50 +0300

    Team 3 (#2)
    * add here the fullName to complete the table and there are no missing fields in the table
    
    * emailjs
    
    * work on single player card
    
    * solved the pass data to component
    
    * v2 of props object
    
    * radarChart
    
    * radar
    
    * test update
    
    * pass props to radar chart
    
    * radar
    
    ---------
    
    Co-authored-by: Elad Abutbul <eladabutbul61@gmail.com>
    Co-authored-by: liatfa <faizliat53@gmail.com>

--- pages/players/index.vue
+++ pages/players/index.vue
@@ -2,16 +2,18 @@
   <Page title="Players" name="players">
     <SectionHeading small>{{ title }}</SectionHeading>
     <v-app>
-      <v-data-table
-        :headers="headers"
-        :items="items"
-        class="elevation-2"
-        hide-default-footer
-        disable-pagination
-        item-key="_id"
-      >
+      <v-data-table :headers="headers" :items="items" class="elevation-2" hide-default-footer disable-pagination
+        item-key="_id">
         <template v-slot:[`item.totalScore`]="{ item }">
           <strong>{{ item.totalScore }}</strong>
+        </template>
+        <template v-slot:[`item.username`]="{ item }">
+          <BaseButton @click="p(item)"  variant="blue">
+            <strong>{{ item.userName }}</strong>
+          </BaseButton>
+        </template>
+        <template v-slot:[`item.fullName`]="{ item }">
+          <strong>{{ item.fullName }}</strong>
         </template>
       </v-data-table>
     </v-app>
@@ -33,12 +35,20 @@ export default {
           text: "Total Score",
           value: "totalScore",
           align: "center",
-          sortable: false
+          sortable: false,
         },
         { text: "Name", value: "fullName" },
-        { text: "Username", value: "username" }
-      ]
+        { text: "Username", value: "username" },
+      ],
     };
+  },
+  methods: {
+    p(item) {
+      this.$router.push({
+        path: `/players/player`,
+        query: { item }
+      });
+    }
   },
   computed: {
     title() {
@@ -46,7 +56,7 @@ export default {
     },
     items() {
       return this.players
-        .filter(player => !!player.username)
+        .filter((player) => !!player.userName)
         .map((player, index) => {
           const item = { ...player };
           for (let key in item) {
@@ -57,12 +67,14 @@ export default {
           item.rank = index + 1;
           return item;
         });
-    }
+    },
+
+
   },
   mounted() {
     // this.io.on("updateTopPlayers", data => {
     //   this.players = data;
     // });
-  }
+  },
 };
 </script>
