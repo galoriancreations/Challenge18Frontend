0d86bc852e2ecfcd35f64a16f8e17aac60ba369d
liatfa <faizliat53@gmail.com>
2023-08-30 19:59:40 +0300

    radar instruction

--- components/player/radarChart.vue
+++ components/player/radarChart.vue
@@ -1,11 +1,17 @@
 <template>
-    <!-- test -->
+   <div>
     <v-radar :stats="stats" :polycolor="polycolor" :radar="radar" :scale="scale">
     </v-radar>
+    <radarInstruction/>
+   </div>
+    <!-- test -->
+
 </template>
+
 
 <script>
 import Radar from 'vue-radar'
+import radarInstruction from './radarInstruction.vue';
 export default {
     data() {
         return {
@@ -15,22 +21,22 @@ export default {
                 radius: '300', // same unit than above (diamètre = 900), keep the radius < (viewbox / 2)
                 structure: {
                     external: { // external stroke of the structure's polygon
-                        strokeColor: 'rgba(0, 0, 0, 1)', // color (any css format is usable (hexa, rgb, rgba...))
+                        strokeColor: 'black', // color (any css format is usable (hexa, rgb, rgba...))
                         strokeWidth: '9' // pixel unit
                     },
                     internals: { // internals stroke of the structure's polygon (one every 10%)
-                        strokeColor: '#76B4FF', // color (any css format is usable (hexa, rgb, rgba...))
+                        strokeColor: 'gray', // color (any css format is usable (hexa, rgb, rgba...))
                         strokeWidth: '5' // pixel unit
                     },
                     average: { // average polygon (placed at 50%)
-                        strokeColor: 'rgba(0, 0, 0, 1)', // stroke color (any css format is usable (hexa, rgb, rgba...))
+                        strokeColor: 'brown', // stroke color (any css format is usable (hexa, rgb, rgba...))
                         strokeWidth: '2', // pixel unit
-                        fillColor: 'rgba(0, 0, 0, .5)' // polygon color (any css format is usable (hexa, rgb, rgba...))
+                        fillColor: 'black' // polygon color (any css format is usable (hexa, rgb, rgba...))
                     }
                 },
                 lines: {  // lines from center to summits
-                    strokeColor: 'rgba(0, 0, 0, .3)', // color (any css format is usable (hexa, rgb, rgba...))
-                    strokeWidth: '1' // pixel unit
+                    strokeColor: 'black', // color (any css format is usable (hexa, rgb, rgba...))
+                    strokeWidth: '6' // pixel unit
                 }
             },
             scale: { // scales of corresponding statistic
@@ -44,13 +50,15 @@ export default {
                 name: stat.type,
                 value: stat.score,
                 level: stat.level,
+                
             })),
 
-            polycolor: '#7307D3' // color (any css format is usable (hexa, rgb, rgba...))
+            polycolor: 'rgba(255, 0, 0, 0.2)' // color (any css format is usable (hexa, rgb, rgba...))
         }
     },
     components: {
-        'v-radar': Radar
+        'v-radar': Radar,
+        radarInstruction
     },
     props: ['item'],
     created() {
