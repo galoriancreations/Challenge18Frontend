9d92136c4756cfee8f80fb92a06be6c02926729a
amitpeer0810 <136604591+amitpeer0810@users.noreply.github.com>
2023-11-07 16:29:10 +0200

    v

---
+++ pages/SharonIsCaring.vue
@@ -0,0 +1,33 @@
+<template>
+    <Page title="Sharon is Caring" name="sharon-is-caring">
+        <BaseButton @click="randomQuestion">Pop a random question</BaseButton>
+        <PopupModal :active="showModal">
+            {{questions[i].text}}
+            <p>
+                share the question
+                Link: 
+            </p>
+        </PopupModal>
+    </Page>
+</template>
+
+<script>
+import questions from "~/assets/data/100questions";
+import popupModal from "~/mixins/popup-modal";
+
+export default {
+    mixins: [popupModal],
+    data() {
+        return { 
+            questions
+        };
+    },
+    methods:{
+        randomQuestion(){
+            showModal = true
+            let i = Math.floor(Math.random() * 95);
+            return i;
+        }
+    }
+}
+</script>
