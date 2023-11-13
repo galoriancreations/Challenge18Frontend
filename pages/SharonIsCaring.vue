8e54f52a6a801ff15afc5e0d2fa3b49844bae0c5
MayNisim <may11zrd@gmail.com>
2023-11-13 13:31:28 +0200

    update

--- pages/SharonIsCaring.vue
+++ pages/SharonIsCaring.vue
@@ -2,11 +2,25 @@
     <Page title="Sharon is Caring" name="sharon-is-caring">
         <BaseButton @click="randomQuestion">Pop a random question</BaseButton>
         <PopupModal :active="showModal">
-            {{questions[i].text}}
-            <p>
-                share the question
-                Link: 
-            </p>
+            {{ questions[i].text }}
+            <div> // לענות תשובה
+                <label for="answer">your answer
+                    <input type="text" placeholder="enter your answer" v-model="answer">
+                </label>
+                <button @click="addAnswer" >share your answer</button>
+            </div>
+            <div> // אם עניתי יש אפשרות לראות עוד תשובות
+                <button @click="getAnswers" >more answers</button>
+                <ul>
+                    <li v-for="x in answers" showAnswer="false">{{ x }}</li>
+                </ul>
+            </div>
+            <div>
+                <p>
+                    share the question
+                    Link:
+                </p>
+            </div>
         </PopupModal>
     </Page>
 </template>
@@ -18,16 +32,27 @@ import popupModal from "~/mixins/popup-modal";
 export default {
     mixins: [popupModal],
     data() {
-        return { 
-            questions
+        return {
+            questions,
+            answer:'',
+            answers:[],
+            showAnswer:false
         };
     },
-    methods:{
-        randomQuestion(){
-            showModal = true
+    methods: {
+        randomQuestion() {
+            showModal = true;
             let i = Math.floor(Math.random() * 95);
             return i;
+        },
+        addAnswer(){
+            this.answers.push(this.answer),
+            this.answer='';
+        },
+        getAnswers(){
+            this.showAnswer = !this.showAnswer
         }
-    }
+    },
+    components: { BaseButton, Page }
 }
 </script>
