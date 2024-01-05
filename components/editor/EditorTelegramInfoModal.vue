3b712e5f84dcc573d8629ef7e8eacdc4cba17149
yoyo3123 <yoav.semo@gmail.com>
2024-01-05 15:54:41 +0200

    changed how info component works

--- components/editor/EditorTelegramInfoModal.vue
+++ components/editor/EditorTelegramInfoModal.vue
@@ -1,21 +1,24 @@
 <template>
     <popup-modal title="Integration Steps" :active="active" :scrollbar="false" style="display: flex; justify-content: flex-start; align-items: start;">
-        <div>
-            <ol id="order_list">
-                <li>Generate an invite link for your group on Ting Global.</li>
-                <li>Create a group on Telegram.</li>
-                <li>Add the Ting Global bot to the Telegram group.</li>
-                <li>In the Telegram group, send the command `/connect` followed by the invite link for Ting Global.</li>
-                <li>Go to the Ting Global group and confirm the connection with Telegram using the command `/telegram`
-                    followed by the link to the Telegram group.
-                </li>
-                <li>Return to the Telegram group and finish the process using the command `/activate` with links for Ting
-                    Global and Telegram groups.
-                </li>
-            </ol>
-        </div>
-        <base-button variant="blue" @click="closeInfoModal" title="Close" class="base-button-info"> Got it </base-button>
+        <div id="container" >
+            <div id="contain" :class="{Right:disableRight,Left:disableLeft}"> 
+            <div  class="items" >
+                <h3 v-if="page > 0" >{{ steps[page-1] }}</h3>
+            </div>
+            <div class="items">
+                <h3  >{{ steps[page]}}</h3>
+            </div>
+            <div v-if="page < 5" class="items" >
+                <h3  >{{ steps[page+1] }}</h3>
+            </div>
+            </div>
+            </div>
 
+
+        <base-button v-if="page > 0" variant="blue" @click="previousPage" title="Back" class="base-button-info">Previous page</base-button>
+
+        <base-button v-if="page < 5" variant="blue" @click="nextPage" title="Next" class="base-button-info">Next page</base-button>
+        <base-button v-else="page < 5" variant="blue" @click="closeInfoModal" title="Close" class="base-button-info">Close</base-button>
     </popup-modal>
 </template>
 
@@ -34,12 +37,42 @@ export default {
     },
 data() {
     return {
-
+        page:0,
+        steps:[
+        'Generate an invite link for your group on Ting Global.',
+        'Create a group on Telegram.',
+        'Add the Ting Global bot to the Telegram group.',
+        'In the Telegram group, send the command `/connect` followed by the invite link for Ting Global.',
+        'Go to the Ting Global group and confirm the connection with Telegram using the command `/telegram followed by the link to the Telegram group.`',
+        'Return to the Telegram group and finish the process using the command `/activate` with links for Ting Global and Telegram groups.'
+        ],
+        disableRight:false,
+        disableLeft:false
     }
 },
     methods: {
         closeInfoModal() {
+            this.page = 0
             this.$emit('toggleInfoModel');
+        },
+        nextPage(){
+            if (!this.disableRight) {
+                this.page++
+                this.disableRight = true
+                setTimeout(()=>{
+                    this.disableRight = false 
+                },750)
+            }
+            // items.style.marginLeft = '300px'
+        },
+        previousPage(){
+            if (!this.disableLeft) {      
+                this.page--
+                this.disableLeft = true
+                setTimeout(()=>{
+                    this.disableLeft = false 
+                },750)
+            }
         }
     }
 
@@ -47,14 +80,71 @@ data() {
 </script>
 
 <style>
-#order_list {
-    text-align: left;
+#contain{
+    position: relative;
+    /* transition: all 1s; */
+    display: flex;
+    flex-direction: row;
+    right: 420px;
+    align-items: center;
+    /* overflow: hidden; */
+    
+}
+
+#container{
+    /* display: flex; */
+    /* flex-direction: row; */
+    overflow: hidden;
+    /* justify-content: center; */
+    margin-bottom: 50px;
+}
+.Left{
+    animation: goLeft 750ms both;
+    transform: translate3d(0, 0, 0);
+}
+.Right{
+
+  animation: goRight 750ms both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes goRight {
+  0%,20% {
+    transform: translate3d(530px, 0, 0);
+  }
+
+  90% {
+    transform: translate3d(0px, 0, 0);
+  }
+
+}
+
+@keyframes goLeft {
+  0%,20% {
+    transform: translate3d(-530px, 0, 0);
+  }
+
+  90% {
+    transform: translate3d(0px, 0, 0);
+  }
+
+}
+
+.items{
+    
+    min-width: 400px;
+    max-width: 400px;
+    /* right: 300px; */
+    margin-right: 130px;
+    /* margin-left: 150px; */
+    
 }
 
 .base-button-info {
     width: 15px;
     height: 50px;
     text-transform: none;
+    display: inline-block;
 }
 
 li {
